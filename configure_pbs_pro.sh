ef902bb25c51e74ef85c77caa3cd374ab0c33f19
Brian May <brian@microcomaustralia.com.au>
2015-03-20 09:22:37 +1100

    Fix typo.

--- configure_pbs_pro.sh
+++ configure_pbs_pro.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-qmgr -c 'create RequireProject RequireProject'
+qmgr -c 'create hook RequireProject'
 qmgr -c 'set hook RequireProject event = queuejob'
 qmgr -c 'import hook RequireProject application/x-python default require_project.py'
