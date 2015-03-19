70a5818c501a3b1718912b4bfb2c019d43b41990
Brian May <brian@microcomaustralia.com.au>
2015-03-19 16:05:06 +1100

    Add required lines.

--- configure_pbs_pro.sh
+++ configure_pbs_pro.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+qmgr -c 'create RequireProject RequireProject'
+qmgr -c 'set hook RequireProject event = queuejob'
 qmgr -c 'import hook RequireProject application/x-python default require_project.py'
